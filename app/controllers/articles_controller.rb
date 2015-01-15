a2940c6fea0bb1c6d4b2d254917c2fe4d04916f7
ThothLogos <naughton.joshua@gmail.com>
2015-01-14 18:21:09 -0800

    Add new/create methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,4 +12,12 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def create
+		@article = Article.new(
+			title: 	params[:article][:title],
+			body:   params[:article][:body] )
+		@article.save
+		redirect_to article_path(@article)		
+	end
+
 end
